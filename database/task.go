f68ca3450a62bd4ea3193fd68033c1d391f61e88
morten <morten@MacBook-Pro-tilhrende-Sofie.local>
2023-03-03 08:50:14 +0100

    .

--- database/task.go
+++ database/task.go
@@ -4,14 +4,28 @@ import "gorm.io/gorm"
 
 type Task struct {
 	gorm.Model
-	Title       *string     `gorm:"not null" json:"title"`
-	Description *string     `gorm:"not null" json:"description"`
-	Images      []TaskImage `json:"images"`
-	UserID      *int        `gorm:"not null" json:"userId"`
-	User        User        `json:"user"`
+	Title          *string         `gorm:"not null" json:"title"`
+	Description    *string         `gorm:"not null" json:"description"`
+	Images         []TaskImage     `json:"images"`
+	UserID         *int            `gorm:"not null" json:"userId"`
+	User           User            `json:"user"`
+	MessageStreams []MessageStream `json:"messageStream"`
+}
+
+type MessageStream struct {
+	gorm.Model
+	Messages []Message
+	TaskID   int `json:"taskId"`
+}
+
+type Message struct {
+	gorm.Model
+	Text            *string `gorm:"not null"`
+	MessageStreamID int     `json:"messageStreamId"`
 }
 
 type TaskImage struct {
 	gorm.Model
-	Uri string `json:"uri"`
+	Uri    string `json:"uri"`
+	TaskID int    `json:"taskId"`
 }
